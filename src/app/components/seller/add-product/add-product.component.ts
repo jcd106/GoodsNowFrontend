fa96e48822f0cdc96fa4383f047efd666d4d2eaa
Angelique Elkins <elkins.angelique@gmail.com>
2018-05-04 12:08:01 -0400

    Revert "added file upload to Add Product"

--- src/app/components/seller/add-product/add-product.component.ts
+++ src/app/components/seller/add-product/add-product.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-import { CloudinaryOptions, CloudinaryUploader } from 'ng2-cloudinary';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-product',
@@ -9,36 +7,14 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-product.component.css']
 })
 export class AddProductComponent implements OnInit {
-  imageIsValid: Boolean = true;
-  imageId: string;
-  uploader: CloudinaryUploader = new CloudinaryUploader(
-    new CloudinaryOptions({ cloudName: 'goodsnow', uploadPreset: 'project2' })
-  );
 
-  constructor(private location: Location, private router: Router) {
-    // Override onSuccessItem to retrieve the imageId
-    this.uploader.onSuccessItem = (item: any, response: string, status: number, headers: any): any => {
-      const res: any = JSON.parse(response);
-      this.imageId = res.public_id;
-      this.router.navigate(['/products']);
-      return { item, response, status, headers };
-    };
-    this.uploader.onErrorItem = (item: any, response: string, status: number, headers: any): any => {
-      this.imageIsValid = false;
-      this.router.navigate(['/products/add']);
-      return { item, response, status, headers };
-    };
-   }
+  constructor(private location: Location) { }
 
   ngOnInit() {
   }
 
   goBack(): void {
     this.location.back();
-  }
-
-  upload() {
-    this.uploader.uploadAll();
   }
 
 }
