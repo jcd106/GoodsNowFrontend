3be39279b07e5fdbdb58f4505b7c0a1bef78c294
Angelique Elkins <angeliqueelkins@csus.edu>
2018-05-05 11:10:03 -0400

    designed browse products page

--- src/app/components/user/browse-products/browse-products.component.ts
+++ src/app/components/user/browse-products/browse-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../../services/products.service';
 
 @Component({
   selector: 'app-browse-products',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./browse-products.component.css']
 })
 export class BrowseProductsComponent implements OnInit {
+  page = 1;
+  products: any[] = new Array(100);
+  category: String = '';
 
-  constructor() { }
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit() {
+    this.productsService.getCategory().subscribe(category => {
+      this.category = category;
+      console.log(this.category);
+    });
+  }
+
+  changeCategory(cat: String) {
+    this.productsService.setCategory(cat);
   }
 
 }
